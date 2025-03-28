8b2ef23433d8145ff129dc531b380067c1b644e7
AnnaNorlyk <148754654+AnnaNorlyk@users.noreply.github.com>
2025-03-28 04:37:58 +0100

    fix

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddEnvironmentVariables();  
 
-
 var dbHost     = Environment.GetEnvironmentVariable("DB_HOST");
-var dbName     = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
-var dbUser     = Environment.GetEnvironmentVariable("MYSQL_USER");
-var dbPassword = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+var dbName     = Environment.GetEnvironmentVariable("DB_NAME");
+var dbUser     = Environment.GetEnvironmentVariable("DB_USER");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
 var connString = $"Server={dbHost};Database={dbName};User={dbUser};Password={dbPassword};";
 
