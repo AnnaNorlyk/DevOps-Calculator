3741b6dbcad16fafb32c4e333daca3bf0af524a7
AnnaNorlyk <148754654+AnnaNorlyk@users.noreply.github.com>
2025-03-28 01:33:55 +0100

    connectionstring

--- API/Program.cs
+++ API/Program.cs
@@ -6,11 +6,18 @@ using Microsoft.AspNetCore.HttpLogging;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Configuration.AddEnvironmentVariables();
 
+builder.Configuration.AddEnvironmentVariables();  
 
-string connString = builder.Configuration.GetConnectionString("DefaultConnection") //i know its not safe 
-    ?? "Server=mariadb;Database=mariadatabase;User=secretuser;Password=secretpassword;";
+var dbHost     = Environment.GetEnvironmentVariable("MYSQL_HOST");
+var dbName     = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
+var dbUser     = Environment.GetEnvironmentVariable("MYSQL_USER");
+var dbPassword = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+
+var connString = $"Server={dbHost};Database={dbName};User={dbUser};Password={dbPassword};";
+
+builder.Services.AddSingleton<HistoryService>(); 
+builder.Services.AddSingleton(new HistoryService(connString)); 
 
 
 builder.Services.AddSingleton(new HistoryService(connString));
@@ -52,3 +59,4 @@ app.UseCors("AllowAll");
 app.MapCalculatorEndpoints();
 
 app.Run();
+public partial class Program { } 
