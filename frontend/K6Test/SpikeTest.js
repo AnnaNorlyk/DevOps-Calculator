ed9098f02855fc6edfa76f6aa05f1283b35322a8
AnnaNorlyk <148754654+AnnaNorlyk@users.noreply.github.com>
2025-04-01 04:27:18 +0200

    k6

---
+++ frontend/K6Test/SpikeTest.js
@@ -0,0 +1,42 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+
+const BASE_URL = 'http://144.24.177.98:5153'; 
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 50 },  
+    { duration: '30s', target: 50 },  
+    { duration: '10s', target: 200 },  
+    { duration: '30s', target: 200 },  
+    { duration: '10s', target: 50 },   
+    { duration: '10s', target: 0 },    
+  ],
+};
+
+export default function () {
+  // Batch hitting "simple" and "cached" endpoints
+  let responses = http.batch([
+    ['GET', `${BASE_URL}/api/simple/add?a=10&b=5`],
+    ['GET', `${BASE_URL}/api/simple/subtract?a=10&b=5`],
+    ['GET', `${BASE_URL}/api/simple/multiply?a=10&b=5`],
+    ['GET', `${BASE_URL}/api/simple/divide?a=10&b=5`],
+    ['GET', `${BASE_URL}/api/simple/factorial?a=10`],
+    ['GET', `${BASE_URL}/api/simple/prime?a=7`],
+
+    ['GET', `${BASE_URL}/api/cached/add?a=10&b=5`],
+    ['GET', `${BASE_URL}/api/cached/subtract?a=10&b=5`],
+    ['GET', `${BASE_URL}/api/cached/multiply?a=10&b=5`],
+    ['GET', `${BASE_URL}/api/cached/divide?a=10&b=5`],
+    ['GET', `${BASE_URL}/api/cached/factorial?a=10`],
+    ['GET', `${BASE_URL}/api/cached/prime?a=11`],
+  ]);
+
+  // Check for HTTP 200
+  responses.forEach((res) => {
+    check(res, { 'status is 200': (r) => r.status === 200 });
+  });
+
+  sleep(1);
+}
